3456a786688a8ffb5984f8b26cc8e44f77956148
Mian-Ahmed-Raza <ahmedmian026@gmail.com>
2025-09-27 12:00:57 +0500

    Modified Media Queries

--- src/Components/Items-Group/Items.jsx
+++ src/Components/Items-Group/Items.jsx
@@ -8,11 +8,11 @@ export default function Items(props) {
         <div className="items">
 
 
+            <img className="img" src={props.imageUrl} alt={props.itemName} />
             <p className="item-name">{props.itemName}</p>
             <span className="from">From </span>
             <br />
             <span className="from">USD {props.price}</span>
-            <img className="img" src={props.imageUrl} alt={props.itemName} />
 
 
         </div>
